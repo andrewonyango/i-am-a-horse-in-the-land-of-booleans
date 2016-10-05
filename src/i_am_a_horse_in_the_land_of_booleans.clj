2024f18b82106c93366360500acc317441bafa07
andrewonyango <andrewonyango@users.noreply.github.com>
2016-10-05 21:03:10 +0300

    i am a horse in the land of booleans

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,25 +2,27 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (= true x))
 
 (defn abs [x]
-  ":(")
+  (if (> 0 x)
+    (- x)
+    (x)))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
   ":(")
 
 (defn teen? [age]
-  ":(")
+  (and (< age 20) (> age 12)))
 
 (defn not-teen? [age]
-  ":(")
+  (and (< age 13) (> age 19)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (* 2 x))
 
 (defn leap-year? [year]
   ":(")
